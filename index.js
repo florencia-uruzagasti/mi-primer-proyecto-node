dddd852d2ff67f8de60feeb037007efdfcbf83f0
Florencia <muruzagasti87@gmail.com>
2022-09-12 07:42:52 -0300

    Primer Ejercicio

---
+++ index.js
@@ -0,0 +1,15 @@
+// Este es el mensaje de Bienvenida
+
+console.log("Esta es la puerta de entrada al proyecto");
+
+/*Este es 
+
+un 
+
+comentario 
+
+diferente*/
+
+// Este es el msj de despedida
+
+console.log("Adiós");
